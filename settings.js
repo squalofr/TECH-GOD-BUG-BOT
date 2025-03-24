03c1d40e5b629e14145f3ab1f55973d10c95560f
squalofr <cesaresturaro@gmail.com>
2025-03-24 15:34:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "393895485784"
+global.ownername = "Squalo"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '393895485784'  //creator number
+global.ownername = '🜏{҉§qµålðñê}҉𐕣⁽⁺³⁹⁾𖤐' //owner name
+global.botname = 'SqualoBot' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
